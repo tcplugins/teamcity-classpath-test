0e934b8a97e62716be257ec498edbc4aed6eeed6
Net Wolf UK <netwolfuk@gmail.com>
2019-03-21 23:32:59 +1300

    Initial commit

---
+++ teamcity-classpath-test-server/src/main/java/teamcity/plugin/classpath/ClassPathTest.java
@@ -0,0 +1,7 @@
+package teamcity.plugin.classpath;
+
+public class ClassPathTest {
+	
+	
+
+}
---
+++ teamcity-classpath-test-server/src/main/java/teamcity/plugin/classpath/ClassPathTestAppServer.java
@@ -0,0 +1,9 @@
+package teamcity.plugin.classpath;
+
+import jetbrains.buildServer.log.Loggers;
+
+public class ClassPathTestAppServer {
+	public ClassPathTestAppServer(ClassPathTest classpath) {
+		Loggers.SERVER.info("ClassPathTestAppServer :: Creating ClassPathTestAppServer instance");
+	}
+}
